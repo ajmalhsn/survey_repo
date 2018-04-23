eaf6b525531f9fa0de9a098802abe4759b47b31d
Ajmal Hussain <ajmalsmarty@gmail.com>
2018-04-23 21:31:22 +0530

    one file change

--- main_input.php
+++ main_input.php
@@ -1,37 +1,47 @@
-
-<!DOCTYPE html>
-<html>
-<title>W3.CSS Template</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-<style>
-body,h1 {font-family: "Raleway", sans-serif}
-body, html {height: 100%}
-.bgimg {
-    background-image: url('/img/hero-bg');
-    min-height: 100%;
-    background-position: center;
-    background-size: cover;
-}
-</style>
-<body>
-
-<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
-  <div class="w3-display-topleft w3-padding-large w3-xlarge">
-    Logo
-  </div>
-  <div class="w3-display-middle">
-    <h1 class="w3-jumbo w3-animate-top">COMING SOON</h1>
-    <hr class="w3-border-grey" style="margin:auto;width:40%">
-    <p class="w3-large w3-center">5 days left</p>
-  </div>
- </div>
-
-</body>
-</html>
 <?php
+include 'connect_db.php';
+$id=0;
+$optionArray;
+$sql="SELECT id FROM screener_survey ORDER BY id DESC LIMIT 1";
+$result=$link->query($sql);
+if($result->num_rows > 0){  
+while($row = $result->fetch_assoc()) {
+    $id=$row["id"];    
+    }
+}
+
+$q17=17;
+$q18=18;
+$stmt2=$link->prepare("INSERT INTO brand_set_ans(fid,qid,brand_value) VALUES(?,?,?)");
+$stmt3=$link->prepare("INSERT INTO brand_set_ans(fid,qid,brand_value) VALUES(?,?,?)");
+
+if (isset($_POST['Q17'])) {
+    $optionArray = $_POST['Q17'];
+    echo "going in <br>";
+}
+for ($i=0; $i<count($optionArray); $i++) {
+        echo $optionArray[$i]."<br/>";
+        //echo mysqli_error($link);
+    
+        $stmt2->bind_param("iii",$id,$q17,$optionArray[$i]);
+        
+        $stmt2->execute();
+        
+}
+if (isset($_POST['Q18'])) {
+    $optionArray = $_POST['Q18'];
+    echo "going in <br>";
+}
+for ($i=0; $i<count($optionArray); $i++) {
+        echo $optionArray[$i]."<br/>";
+        //echo mysqli_error($link);
+    
+        $stmt2->bind_param("iii",$id,$q18,$optionArray[$i]);
+        
+        $stmt2->execute();
+
+}
+
 //echo "everything worked successfully"
 //sleep(5);
 //header("Location:IRT.php");
